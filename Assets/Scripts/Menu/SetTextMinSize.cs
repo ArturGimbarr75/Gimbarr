93c6c0bba6599c52580039f6a1144a6873f816d3
Artur <Artur.Radzivilov@stud.vgtu.lt>
2021-02-25 13:24:48 +0200

    Added "WorkoutList" scene with title auto-resize

---
+++ Assets/Scripts/Menu/SetTextMinSize.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetTextMinSize : MonoBehaviour
+{
+    [SerializeField]
+    private Text[] TextArr;
+
+    void Start()
+    {
+        StartCoroutine("SetTextFontSize");
+    }
+
+    IEnumerator SetTextFontSize()
+    {
+        if (TextArr.Length != 0)
+        {
+            yield return new WaitForSecondsRealtime(2);
+
+            int minFontSize = TextArr.First().cachedTextGenerator.fontSizeUsedForBestFit;
+            foreach (var t in TextArr)
+            {
+                t.resizeTextForBestFit = false;
+                if (t.cachedTextGenerator.fontSizeUsedForBestFit < minFontSize)
+                {
+                    minFontSize = t.cachedTextGenerator.fontSizeUsedForBestFit;
+                }
+            }
+            foreach (var t in TextArr)
+            {
+                t.resizeTextForBestFit = false;
+                t.fontSize = minFontSize;
+            }
+        }
+        yield return null;
+    }
+
+
+}
