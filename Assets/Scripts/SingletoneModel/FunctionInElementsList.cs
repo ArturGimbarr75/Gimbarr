9606ac7f9d51b902994215c25bde8f83275617b0
Artur <Artur.Radzivilov@stud.vgtu.lt>
2021-02-25 21:30:29 +0200

    New singletonemodel

---
+++ Assets/Scripts/SingletoneModel/FunctionInElementsList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.SingletoneModel
+{
+    class FunctionInElementsList
+    {
+        public static FunctionInElementsList Instance { get; private set; }
+        public FunctionType Function { get; set; }
+
+        static FunctionInElementsList()
+        {
+            Instance = new FunctionInElementsList()
+            {
+                Function = FunctionType.Info
+            };
+        }
+
+        public enum FunctionType
+        {
+            Info,
+            Choice
+        }
+    }
+}
