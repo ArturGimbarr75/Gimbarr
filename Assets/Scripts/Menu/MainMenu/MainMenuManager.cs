c40aa2c824c386f8b74da4463e49adaec3c62ace
Artur <Artur.Radzivilov@stud.vgtu.lt>
2021-02-26 20:31:34 +0200

    Statistics of elements count in "MainMenu" scene

---
+++ Assets/Scripts/Menu/MainMenu/MainMenuManager.cs
@@ -0,0 +1,25 @@
+﻿using Assets.Scripts.ModelControllers;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuManager : MonoBehaviour
+{
+    [SerializeField]
+    private Text Statistics;
+
+    void Start()
+    {
+        var line = "Giros: {0}\nFiguras: {1}\nYoyos: {2}";
+        Statistics.text = String.Format
+        (
+            line,
+            GimbarrElements.AllElements.Count(x => x.Style == GimbarrElements.GimbarrStyle.Giros),
+            GimbarrElements.AllElements.Count(x => x.Style == GimbarrElements.GimbarrStyle.Figuras),
+            GimbarrElements.AllElements.Count(x => x.Style == GimbarrElements.GimbarrStyle.Yoyos)
+        );
+    }
+}
