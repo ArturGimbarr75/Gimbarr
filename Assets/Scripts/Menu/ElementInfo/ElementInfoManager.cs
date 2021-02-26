2101262284aac4f587f5acf9645e16bd0f6d4f43
Artur <Artur.Radzivilov@stud.vgtu.lt>
2021-02-26 15:09:39 +0200

    New button in "ElementInfo" scene

--- Assets/Scripts/Menu/ElementInfo/ElementInfoManager.cs
+++ Assets/Scripts/Menu/ElementInfo/ElementInfoManager.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections;
+﻿using Assets.Scripts.DataBase.WorkoutElementTableNS;
+using Assets.Scripts.DataBase.WorkoutTableNS;
+using Assets.Scripts.SingletoneModel;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,10 +9,29 @@ public class ElementInfoManager : MonoBehaviour
 {
     [SerializeField]
     private LoadingManager Loading;
+    [SerializeField]
+    private GameObject AddButton;
+
+    void Start()
+    {
+        if (FunctionInElementsList.Instance.Function == FunctionInElementsList.FunctionType.Info)
+            AddButton.SetActive(false);
+        else
+            AddButton.SetActive(true);
+    }
 
     void Update()
     {
         if (Input.GetKeyUp(KeyCode.Escape))
             Loading.StartSceneLoading(1);
+    }
+
+    public void OnAddClick()
+    {
+        if (WorkoutTable.HasUnfinishedWorkout())
+        {
+            WorkoutElementTable.AddElementToWorkout(SelectedElement.Instance.Selected.ID);
+            Loading.StartSceneLoading(6);
+        }
     }
 }
