7bd4ac00a267d53cdfb78fcc5ee422e1632798a0
Artur <Artur.Radzivilov@stud.vgtu.lt>
2021-03-10 01:19:30 +0200

    Added video loading animation

--- Assets/Scripts/Menu/ElementInfo/SelectVideo.cs
+++ Assets/Scripts/Menu/ElementInfo/SelectVideo.cs
@@ -6,8 +6,23 @@ using YoutubePlayer;
 
 public class SelectVideo : MonoBehaviour
 {
+    [SerializeField]
+    GameObject Loading;
+    [SerializeField]
+    GameObject Panel;
+    [SerializeField]
+    VideoPlayerProgress Progress;
+    SimpleYoutubeVideo SYV;
+
     void Awake()
     {
-        GetComponent<SimpleYoutubeVideo>().videoUrl = SelectedElement.Instance.Selected.Url;
+        SYV = GetComponent<SimpleYoutubeVideo>();
+        SYV.videoUrl = SelectedElement.Instance.Selected.Url;
+    }
+
+    void Update()
+    {
+        if (Progress.videoPlayer.isPrepared && Panel.activeSelf)
+            Loading.SetActive(false);
     }
 }
